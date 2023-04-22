88b312801c607c237c6c0c1e7723ff220ba076e5
Graham Waters <graham.waters.business@gmail.com>
2023-04-22 17:53:00 -0500

    The changes made in this version include:
    Removing the global variable page and passing it as an argument to functions.
    Adding a new function get_additional_info to extract more details from the output screen.
    Modifying the run function to return both name and additional_info.
    Printing the name and additional_info in the main function.
    Using f-strings to format the output.

--- Investigation/FindOwner.py
+++ Investigation/FindOwner.py
@@ -2,7 +2,8 @@ import asyncio
 from playwright.async_api import async_playwright
 from pyvirtualdisplay import Display
 import time
-async def GoogleMail(email,password):
+
+async def google_mail(page, email, password):
     try:
         await page.locator("#identifierId").fill(email)
         await page.click("#identifierNext > div > button > span")
@@ -14,18 +15,24 @@ async def GoogleMail(email,password):
             await page.click("#submit_approve_access > div:nth-child(1) > button:nth-child(1) > div:nth-child(1)")
     except:
         pass
-async def getName():
-    global name
+
+async def get_name(page):
     try:
-        name = await page.text_content("#app > main > div > div > div > div.rounded-xl.overflow-hidden.shadow > header > div:nth-child(1) > div.font-montserrat.text-lg.sm\:text-2xl.flex-none",timeout=10000)
+        name = await page.text_content("#app > main > div > div > div > div.rounded-xl.overflow-hidden.shadow > header > div:nth-child(1) > div.font-montserrat.text-lg.sm\:text-2xl.flex-none", timeout=10000)
     except:
         name = await page.text_content("#app > main > div > div > div > div.flex.items-center.gap-4.mb-4 > div > h3")
         if "Oops! Search limit exceeded." in name:
-            name="exceeded"
-def printName():
+            name = "exceeded"
     return name
-async def run(playwright,phone_number,email,password):
-    global page
+
+async def get_additional_info(page):
+    try:
+        additional_info = await page.text_content("#app > main > div > div > div > div.rounded-xl.overflow-hidden.shadow > header > div:nth-child(2) > div.font-montserrat.text-lg.sm\:text-2xl.flex-none", timeout=10000)
+    except:
+        additional_info = ""
+    return additional_info
+
+async def run(playwright, phone_number, email, password):
     display = Display(visible=0, size=(1600, 1200))
     display.start()
     firefox = playwright.firefox
@@ -35,9 +42,16 @@ async def run(playwright,phone_number,email,password):
     await page.locator("#app > main > header > div > form > input").fill(phone_number)
     await page.locator("#app > main > header > div > form > button").click()
     await page.locator("#app > main > div > div > a:nth-child(2)").click()
-    await GoogleMail(email,password)
-    await getName()
+    await google_mail(page, email, password)
+    name = await get_name(page)
+    additional_info = await get_additional_info(page)
     await browser.close()
-async def main(phone_number,email,password):
+    display.stop()
+    return name, additional_info
+
+async def main(phone_number, email, password):
     async with async_playwright() as playwright:
-        await run(playwright,phone_number,email,password)
+        name, additional_info = await run(playwright, phone_number, email, password)
+        print(f"Name: {name}")
+        if additional_info:
+            print(f"Additional Info: {additional_info}")
